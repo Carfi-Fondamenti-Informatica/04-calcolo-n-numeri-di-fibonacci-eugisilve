b3e975bcf9f6eeecfabc0362b65823b8d605eb8a
eugisilve <114446323+eugisilve@users.noreply.github.com>
2022-11-24 23:34:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0;
+    int a=1 ;
+    int b=1;
+    int c=0;
+    cin >> n;
+    if ( n>=2) {
+        cout << a << endl << b << endl;
+        for ( int i = 2; i< n; i++) {
+            c= a+ b;
+            a=b;
+            b=c;
+            cout << c << endl;
+        }
+    }
+    else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
